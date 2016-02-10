5060e8a9804ec004eadd12a6b57fd3e008834ef9
Jason Noble <github+jasonn@jasonnoble.org>
2016-02-10 11:52:29 -0700

    End of video

--- HotMess/ViewController.swift
+++ HotMess/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var displayLabel: UILabel!
+  @IBOutlet weak var nameTextField: UITextField!
+
+  @IBOutlet weak var redSlider: UISlider!
+  @IBOutlet weak var greenSlider: UISlider!
+  @IBOutlet weak var blueSlider: UISlider!
+
+  @IBAction func sayButtonTouched(sender: UIButton) {
+    // grab test from nameText Field
+    // Set displayLabel to "Hello " ^
+
+    if let name = nameTextField.text {
+      displayLabel.text = "Hello \(name)"
+    }
+    nameTextField.resignFirstResponder()
+  }
+
+  @IBAction func sliderChanged(slider: UISlider) {
+    setColor()
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    displayLabel.text = ""
+    setColor()
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +42,20 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  func setColor() {
+    view.backgroundColor = UIColor(
+      red: CGFloat(redSlider.value),
+      green: CGFloat(greenSlider.value),
+      blue: CGFloat(blueSlider.value),
+      alpha: 1.0
+    )
+  }
+}
 
+extension ViewController : UITextFieldDelegate {
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return true
+  }
 }
 
